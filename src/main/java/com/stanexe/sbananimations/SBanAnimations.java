ce3b98f0fd26832c709056c3888c206e58a102aa
Stanned <stanexe1717@gmail.com>
2021-12-03 16:43:27 +0100

    Initial Commit

---
+++ src/main/java/com/stanexe/sbananimations/SBanAnimations.java
@@ -0,0 +1,17 @@
+package com.stanexe.sbananimations;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SBanAnimations extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
